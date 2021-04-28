8873fccfff590ec8f4db72f1f285486cce189e3a
Christian Zintel <c.zintel@yahoo.de>
2021-04-28 15:26:43 +0200

    finished exercize

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 const form = document.querySelector('[data-form]')
-const input = document.querySelector('[name="uppercase-text"]')
-const output = document.querySelector('[data-output]')
+const input1 = document.querySelector('[name="input1"]')
+const input2 = document.querySelector('[name="input2"]')
 
 form.addEventListener('submit', event => {
   event.preventDefault()
-  output.innerText = input.value.toUpperCase()
-  input.value = ''
-  input.focus()
+  const inputText1 = input1.value
+  const inputText2 = input2.value
+  input1.value = inputText2.toUpperCase()
+  input2.value = inputText1.toLowerCase()
 })
